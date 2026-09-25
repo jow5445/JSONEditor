331b46866021f65cc56c01c1808975eb5c9f0d23
Youssef Ahmed <yousef.ahmed.dev@outlook.com>
2026-09-25 20:58:38 +0300

    Cleans up project files and code comments

--- json-editor.js
+++ json-editor.js
@@ -48,7 +48,6 @@ class JSON_Editor extends HTMLElement {
         this.value = this.getAttribute('value')
     }
 
-    //===[ Caret Control ]=================================================
 
     get_selection() {
         if( this.shadowRoot.getSelection )
@@ -56,7 +55,6 @@ class JSON_Editor extends HTMLElement {
         return document.getSelection()
     }
 
-    // return a "pointer" with relevant information about the caret position
     get_caret_pointer() {
         const selection = this.get_selection()
         if (selection.rangeCount > 0) {
@@ -72,7 +70,6 @@ class JSON_Editor extends HTMLElement {
         return null
     }
 
-    // set the caret position based on pointer information
     set_caret_from_pointer(pointer) {
         const selection = window.getSelection()
         const range = document.createRange()
@@ -96,25 +93,20 @@ class JSON_Editor extends HTMLElement {
         selection.addRange(range)
     }
 
-    //===[ Utils ]=========================================================
 
-    // escape string special characters used in regular expressions
     escape_regex_string(string) {
         return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
     }
 
-    // return the position of the occurrence-th sub_string occurrence
     get_position_of_occurrence(string, sub_string, occurrence) {
         const position = string.split(sub_string, occurrence).join(sub_string).length
         return position === string.length ? -1 : position
     }
 
-    // return the number of sub_string occurrences
     get_number_of_occurrences(string, sub_string) {
         return sub_string ? string.replace(new RegExp(`[^${this.escape_regex_string(sub_string)}]`, 'g'), '').length : 0
     }
 
-    // return the element's children text nodes
     get_text_nodes(element) {
         let node, list=[], walk=document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false)
         while(node=walk.nextNode())
@@ -122,16 +114,13 @@ class JSON_Editor extends HTMLElement {
         return list
     }
 
-    //===[ Formatting ]====================================================
 
     escape_html(input) {
         const replace = [ ['&', '&amp;'], ['<', '&lt;'], ['>', '&gt;'], ['"', '&quot;'], ["'", '&#039;'] ]
         return replace.reduce( ( escaped, replacement) => escaped.replaceAll( ...replacement ), input)
     }
 
-    // format a json object
     format_object(input, offset=0) {
-        // in JS typeof null returns "object" (legacy bug), for null input we just return null
         if( input === null )
             return '<span part="null">null</span>'
         let output = ''
@@ -144,7 +133,6 @@ class JSON_Editor extends HTMLElement {
         return output
     }
 
-    // format a json array
     format_array(input, offset=0) {
         let output = ''
         output += `<span part="brackets">[</span><br>\n`
@@ -156,17 +144,14 @@ class JSON_Editor extends HTMLElement {
         return output
     }
 
-    // format a json string
     format_string(input) {
         return `<span part="string"><span part="string_quotes">\"</span>${this.escape_html(input)}<span part="string_quotes">\"</span></span>`;
     }
 
-    // format a boolean
     format_boolean(input) {
         return `<span part="${input}">${input}</span>`;
     }
 
-    // format a number
     format_number(input) {
         return `<span part="number">${input}</span>`;
     }
@@ -201,7 +186,6 @@ class JSON_Editor extends HTMLElement {
             return
         }
 
-        // prevent unnecesary render
         const current_string_content = JSON.stringify(content)
         if(!content || current_string_content === this.last_string_content)
             return
@@ -211,8 +195,6 @@ class JSON_Editor extends HTMLElement {
         if(pointer && focus)
             this.set_caret_from_pointer(pointer)
     }
-
-    //===[ Getters / Setters ]=============================================
 
     get raw_string() {
         // remove %A0 (NBSP) characters, which are no valid in JSON
